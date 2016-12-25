6a5ee1d381c0ff8db79e073dab3f3555dfe2f10c
LeonLH <laohan1221@126.com>
2016-12-25 11:48:42 +0800

    Update Discribe of DataStruction

--- C++ Reviewed Program/Data Struction/ReadMe.cpp
+++ C++ Reviewed Program/Data Struction/ReadMe.cpp
@@ -1,14 +1,11 @@
-#include"MyList.h"
 
-void main()
-{
 //关于C++数据结构主要要掌握的知识为：
 //1.MFC中数据结构的编写：（要求编写出来）
 //	->CList
 //	->CArray（动态数组）
-//	->CMap
+//	->CMap(Hash)
 //	->CString
-//	->
+//	->CMap(BinTree)
 //2.STL中的STL Containers Iterators Algorithms Allocators Function_Objects
 //其中重点是：Containers Iterators algorithms
 //	Containers中：（要求编写出来）
@@ -20,4 +17,4 @@ void main()
 //					->queue
 //					->stack
 
-}
+
